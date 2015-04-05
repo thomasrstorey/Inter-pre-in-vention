9bca10bd8cb8c61ad813d8ac684d43cb60e8835a
thomasrstorey <thomasrstorey@cfa-fac-302-2.ad.ufl.edu>
2015-04-04 20:04:10 -0400

    first version of tree visualization seems to be working with random, dummy data

--- frontend/app/components/tree/treeCtrl.js
+++ frontend/app/components/tree/treeCtrl.js
@@ -6,16 +6,17 @@ angular.module('TreeCtrl', [])
 			$scope.nodes = [];
 			$scope.links = [];
 			$scope.distsList = [];
+			$scope.dists = [];
 			//Format JSON for d3 Force
 			$http.get('/api/poems_list/')
 				.success(function (data) {
 					$scope.poemsList = data;
 					$scope.nodes = _.map($scope.poemsList, function (val, key, arr) {
-						return {pid: val.pid, title: val.title};
+						return {pid: val.pid, title: val.title, dist: null};
 					});
 					$scope.links = _.flatten(_.map($scope.poemsList, function (src) {
 						return _.map(src.children, function (child) {
-							return {source: src.pid, target: child.pid, value: child.dist};
+							return {source: src.pid, target: child.pid, value: child.dist, display: true};
 						});
 					}));				
 				})
@@ -26,7 +27,6 @@ angular.module('TreeCtrl', [])
 			$http.get('/api/dists_list')
 				.success(function (obj) {
 					$scope.distsList = obj.data;
-					console.log($scope.distsList);
 				})
 				.error(function (err) {
 					console.log("Error: " + err);
@@ -38,27 +38,25 @@ angular.module('TreeCtrl', [])
 		[function () { //factory function for tree-canvas directive
 			return {
 				link: function (scope, elem, attr) {
-					var l = scope.nodes.length;
+					var l = scope.links.length;
 					var width = window.innerWidth,
 						height = window.innerHeight;
-					var force = d3.layout.force()
-						.charge(-30)
-						.gravity(0.1)
-						.linkDistance(function (d) { return d.value })
-						.size([width, height]);
+					var force = d3.layout.force();
 					var svg = d3.select("body").append("svg")
 						.attr("width", width)
 						.attr("height", height);
 					var link = svg.selectAll(".link");
 					var node = svg.selectAll(".node");
-
+					scope.$watchGroup(['nodes','links'], function () {
+						console.log(scope.links);
+						update();
+					}, true);
+					
+					
 					node.append("title")
 						.text(function (d) { return d.title });
 					force.on("tick", function() {
-						if(scope.nodes.length != l){
-							l = scope.nodes.length;
-							update();
-						}
+
 					    link.attr("x1", function(d) { return d.source.x; })
 					        .attr("y1", function(d) { return d.source.y; })
 					        .attr("x2", function(d) { return d.target.x; })
@@ -66,6 +64,7 @@ angular.module('TreeCtrl', [])
 
 					    node.attr("cx", function(d) { return d.x; })
 					        .attr("cy", function(d) { return d.y; });
+
 				    });
 
 				    function update () {
@@ -73,23 +72,45 @@ angular.module('TreeCtrl', [])
 				    	force
 				    		.nodes(scope.nodes)
 						    .links(scope.links)
+						    .charge(-30)
+						.chargeDistance(500)
+						.gravity(0.1)
+						.linkDistance(function (d) { return d.value })
+						.linkStrength(function (d) { return d.display ? 0 : 1.0})
+						.size([width, height])
 						    .start();
 
-						link = link.data(scope.links)
+						link = link.data(scope.links);
+						link
 							.enter().append("line")
 							.attr("class", "link")
 							.style("stroke-width", 1)
 						    .style("stroke", "black");
-
+						link
+							.exit()
+							.remove();
+						
 						node = node.data(scope.nodes)
 							.enter().append("circle")
 							.attr("class", "node")
 							.attr("r", 5)
 							.style("fill", "#000")
 							.on("click", function (d) {
-								node.each(function (d) {
-									if(d.fixed){
-										d.fixed = false;
+								console.log(scope.links);
+								_.remove(scope.links, function (l) { return l.display == false });
+								console.log(scope.links);
+								_.each(_.map(scope.distsList[d.pid].dists, function (link) {
+									return {source: d.pid, target: link.pid, value: link.val, display: false};
+								}), function (link) {
+									scope.links.push(link);
+								});
+
+								console.log(scope.links);
+								scope.$apply();
+								node.each(function (ed) {
+									ed.dist = scope.distsList[d.pid].dists[ed.pid].val;
+									if(ed.fixed){
+										ed.fixed = false;
 								    }
 								});
 								d.fixed = true;
@@ -97,9 +118,7 @@ angular.module('TreeCtrl', [])
 								d.py = height/2;
 								d.x = width/2;
 								d.y = height/2;
-								force.resume();
-								console.log("x: " + d.x);
-								console.log("y: " + d.y);
+								force.start();
 							});
 				    }
 				    update();
